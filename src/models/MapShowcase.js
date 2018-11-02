07b7448e0c9ea9e65ad55d29717382dd2f3ba0c0
VelazquezLuis <javiervelazquez113@yahoo.com>
2018-11-02 08:58:09 -0700

    added markers to each location

--- src/models/MapShowcase.js
+++ src/models/MapShowcase.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import {Map, GoogleApiWrapper, InfoWindow} from 'google-maps-react';
 
 const api_Key = "AIzaSyAM4l2gAoyd3OxMlqhkICZ_IfFoQ1E-Uds";
 
 class MapShowcase extends Component {
-  //hold map object
+  //holds map object
   state = {
-    map: null
+    map: null,
+    markers: [],
+    markerProps: [],
+    activeMarker: null,
+    activeMarkerProps: null,
+    showingInfoWindow: false
+
   };
 
 
@@ -16,8 +22,58 @@ class MapShowcase extends Component {
 
   //once map has loaded this will execute and pass the map object 
   mapReady = (props,map) => {
-    //savethe map refrence in state and prepare the location  makrkers 
+    //savethe map refrence in state and prepare the location  markers 
     this.setState({map});
+    this.updateMarkers(this.props.locations);
+  }
+
+  //close markers animation
+  closeInfoWindow = () => {
+    this.state.activeMarker && this
+      .state
+      .activeMarker
+      .setAnimation(null);
+    this.setState({showingInfoWindow: false, activeMarker: null, activeMarkerProps: null});    
+  }
+
+  //closes all info windows 
+  onMarkerClick = (props, marker, e) => {
+    this.closeInfoWindow();
+
+    this.setState({showingInfoWindow:true ,activeMarker:marker, activeMarkerProps: props});
+  }
+
+  //handles location markers after filtering
+  updateMarkers = (locations) => {
+    if(!locations)
+      return;
+    
+    //clears out markers first 
+    this.state.markers.forEach(marker => marker.setMap(null));
+
+    let markerProps =[];
+    let markers = locations.map((location, index) => {
+      let mProps = { // change name to temp maeker props
+        key:index,
+        index,
+        name: location.name,
+        position: location.pos,
+        url: location.url
+      };
+      markerProps.push(mProps);
+
+      let animation = this.props.google.maps.Animation.DROP;
+      let marker = new this.props.google.maps.Marker({
+        position: location.pos,
+        map: this.state.map,
+        animation
+      });
+      marker.addListener('click', () => {
+        this.onMarkerClick(mProps, marker, null);
+      });
+      return marker;
+    })
+    this.setState({markers,markerProps});
   }
 
   // rednders the mab object
@@ -31,6 +87,8 @@ class MapShowcase extends Component {
       lat: this.props.lat,
       lng: this.props.lng
     }
+    let amProps = this.state.activeMarkerProps; //cnage amprps to activemarkerprops
+
     return (
       <Map
         role="application"
@@ -41,6 +99,16 @@ class MapShowcase extends Component {
         style={style}
         initialCenter={center}
         onClick={this.closeInfoWindow}>
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.closeInfoWindow}>
+          <div>
+            <h3>{amProps &&  amProps.name} </h3>
+            {amProps && amProps.url ? (<a href={amProps.url}>See Website</a> )
+            : ""}
+          </div>
+          </InfoWindow>
       </Map>
     )
   }
