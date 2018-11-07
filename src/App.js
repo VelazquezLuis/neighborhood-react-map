1c570697b8ca9fb3fcfe952679b2f28da801f76a
VelazquezLuis <javiervelazquez113@yahoo.com>
2018-11-07 14:27:55 -0800

    updateed side bar

--- src/App.js
+++ src/App.js
@@ -10,42 +10,16 @@ class App extends Component {
     lng: -115.1884364,
     zoom: 14,
     all: locations, // change all to somthing similliar 
-    filtered:null,
+    filtered: null,
     open: false
 
   }
-
-  styles = {
-    menuButton: {
-      marginLeft: 10,
-      marginRight: 20,
-      position: "absolute",
-      left: 10,
-      top: 20,
-      background: "white",
-      padding:10
-    },
-    hide: {
-      display: 'none'
-    },
-    header: {
-      marginTop:"0px"
-    }
-  };
-
   componentDidMount = () => {
     this.setState({
       ...this.state,
       filtered: this.filterLocations(this.state.all, "")
     });
-  }
-
-  //shows the list
-  toggleDrawer = () => {
-    this.setState({
-      open: !this.state.open
-    });
-  }
+  }  
 
   //refresh the query string
   updateQuery = (query) => {
@@ -69,29 +43,25 @@ class App extends Component {
   //renders google map into the browser 
   render = () => {
     return (
-      <div className="App">
-        <div>
-          <button onClick={this.toggleDrawer} style={this.styles.menuButton}>
-            <i className= "fa fa-bars"></i>
-          </button>          
+      <section className="App">
+        <section>                    
           <h1>
               Vegas Casinos & Hotels
           </h1>          
-        </div>
+        </section>
         <MapShowcase
           lat={this.state.lat}
           lng={this.state.lng}
           zoom={this.state.zoom}
           locations={this.state.filtered}
           selectedIndex={this.state.selectedIndex}
-          clickListItem={this.clickListItem}/>
+          clickListItem={this.clickListItem}
+          />
         <ListDD 
           locations={this.state.filtered}
-          open={this.state.open}
-          toggleDrawer={this.toggleDrawer}
           filterLocations= {this.updateQuery}
           clickListItem={this.clickListItem}/>
-      </div>
+      </section>
     );
   }
 }
